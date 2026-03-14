10c98401a0840ad5dee448d3ef463b2c19d4ca29
Salah <nh.salah@gmail.com>
2026-03-15 01:15:19 +0400

    Update rewordit download URL to Cloudflare R2

--- Casks/rewordit.rb
+++ Casks/rewordit.rb
@@ -2,7 +2,7 @@ cask "rewordit" do
   version "0.1.0"
   sha256 "4abb9fa9fa8dbd72736959fae7d10f972f9f3e68b55768e441d5cd71a1192eee"
 
-  url "https://github.com/spichen/rewordit/releases/download/v#{version}/RewordIt-#{version}.dmg"
+  url "https://pub-4b86e3f39bea468fa3fb2854fe2c7bab.r2.dev/RewordIt-#{version}.dmg"
   name "RewordIt"
   desc "macOS menu bar utility that rewords selected text using LLMs"
   homepage "https://github.com/spichen/rewordit"
